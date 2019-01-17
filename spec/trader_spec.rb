8133e1a1ebe92872e12b50296d79d1f00ab0debc
Laspargus <maxime.speroni@gmail.com>
2019-01-17 15:14:23 +0100

    ajout exos 1 et 2

---
+++ spec/trader_spec.rb
@@ -0,0 +1,24 @@
+
+require_relative '../lib/trader'
+
+describe "Mon programme de scrapping Crypto fonctionne-t-il ?" do
+    it "Le programme doit me retourner un array" do
+      expect(crypto_scrapper.is_a?Hash).to eq(true)
+     end
+
+    it "Le tableau doit contenir à minima 100 valeurs" do
+        expect(crypto_scrapper.length>100).to eq(true)
+       end
+
+    it "Le programme doit contenir à minima le Symbol BTC" do
+        expect(crypto_scrapper.include?('BTC')).to eq(true)   
+      end
+
+    it "Le programme doit contenir à minima le Symbol BTC" do
+        expect(crypto_scrapper['BTC'].delete_prefix('$').to_i>3000).to eq(true)   
+      end
+
+
+  end
+
+  
